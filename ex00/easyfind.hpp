57ceba9f7e15215c17a1fd50bb94f34f6c2f3323
clsaad <clsaad@student.42lyon.fr>
2021-11-16 14:16:21 +0100

    ex00 : fuck off

--- ex00/easyfind.hpp
+++ ex00/easyfind.hpp
@@ -1,11 +1,8 @@
 #pragma once
 
+#include <algorithm>
+
 template<typename _Tp>
 typename _Tp::iterator easyfind(_Tp &container, int val) {
-	for (typename _Tp::iterator it = container.begin(); it != container.end(); ++it) {
-		if (*it == val) {
-			return it;
-		}
-	}
-	return container.end();
+	return std::find(container.begin(), container.end(), val);
 }
